090adc35063fea30248caa3566090131c706e582
Leonardo Abril <leoabril98@gmail.com>
2024-02-07 06:22:50 -0300

    adicionando tryCatch

--- main.js
+++ main.js
@@ -12,12 +12,19 @@ document.addEventListener('DOMContentLoaded', function() {
         return res.json();
     })
     .then(function(json) {
-        nameElement.innerText = json.name;
-        usernameElement.innerText = json.name;
-        avatarElement.src = json.avatar_url;
-        followingElement.innerText = json.following;
-        followersElement.innerText = json.followers;
-        reposElement.innerText = json.public_repos;
-        linkElement.href = json.html_url;
+        try {
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.name;
+            avatarElement.src = json.avatar_url;
+            followingElement.innerText = json.following;
+            followersElement.innerText = json.followers;
+            reposElement.innerText = json.public_repos;
+            linkElement.href = json.html_url;
+        } catch (error) {
+            console.error('Erro ao processar os dados da API:', error);
+        }
     })
+    .catch(function(error) {
+        console.error('Erro ao buscar os dados da API:', error);
+    });
 })
