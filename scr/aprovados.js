c7ae4430366849bd60b6e6757370b43c46e7da8b
Leonardo Abril <leoabril98@gmail.com>
2024-02-12 20:55:06 -0300

    primeiro commit

---
+++ scr/aprovados.js
@@ -0,0 +1,20 @@
+// Criar um array de objetos representando alunos
+const alunos = [
+    { nome: 'João', nota: 7 },
+    { nome: 'Maria', nota: 5 },
+    { nome: 'Pedro', nota: 8 },
+    { nome: 'Ana', nota: 6 },
+    { nome: 'Carlos', nota: 4 }
+];
+
+// Função para filtrar alunos com nota maior ou igual a 6
+function filtrarAlunosAprovados(listaAlunos) {
+    return listaAlunos.filter(aluno => aluno.nota >= 6);
+}
+
+// Chamar a função para filtrar os alunos aprovados
+const alunosAprovados = filtrarAlunosAprovados(alunos);
+
+// Exibir os alunos aprovados
+console.log('Alunos aprovados:');
+console.log(alunosAprovados);
