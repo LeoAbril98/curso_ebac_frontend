03846d57bfbdb3bb05fd59238978a4abc0703a0c
Leonardo Abril <leoabril98@gmail.com>
2024-02-14 21:34:35 -0300

    alterando valores

--- src/multiplicacao.ts
+++ src/multiplicacao.ts
@@ -8,6 +8,5 @@ function saudacao(nome: string): string {
     return "Olá " + nome;
 }
 
-// Exemplos de uso das funções
-console.log(multiplicacao(3, 15)); // Saída: 12
-console.log(saudacao("Leonardo")); // Saída: Olá Joãonode
+console.log(multiplicacao(3, 15)); 
+console.log(saudacao("Leonardo"));
