0c4100d8e4309e19d5985369802f3dd8a18fba99
Leonardo Abril <leoabril98@gmail.com>
2024-02-14 21:31:20 -0300

    first commit

---
+++ src/multiplicacao.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso das funções
+console.log(multiplicacao(3, 15)); // Saída: 12
+console.log(saudacao("Leonardo")); // Saída: Olá Joãonode
