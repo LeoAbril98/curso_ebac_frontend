f86f66fc96cfbf265065216a3c705e931c4c5fb8
Leonardo Abril <leoabril98@gmail.com>
2024-02-14 21:35:16 -0300

    alterando valores

--- src/multiplicacao.js
+++ src/multiplicacao.js
@@ -7,6 +7,5 @@ function multiplicacao(num1, num2) {
 function saudacao(nome) {
     return "Olá " + nome;
 }
-// Exemplos de uso das funções
-console.log(multiplicacao(3, 15)); // Saída: 12
-console.log(saudacao("Leonardo")); // Saída: Olá Joãonode
+console.log(multiplicacao(3, 15));
+console.log(saudacao("Leonardo"));
